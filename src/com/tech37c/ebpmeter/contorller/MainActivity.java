15a8b59b4e47b2439b53bcc08cede3c42b6acca8
lxsbox <lxsbox@gmail.com>
2014-11-09 09:32:32 +0800

    11.9

--- src/com/tech37c/ebpmeter/contorller/MainActivity.java
+++ src/com/tech37c/ebpmeter/contorller/MainActivity.java
@@ -6,6 +6,8 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import com.tech37c.ebpmeter.R;
 import com.tech37c.ebpmeter.model.BusinessHandler;
@@ -43,6 +45,7 @@ import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.NavUtils;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -260,4 +263,38 @@ public class MainActivity extends FragmentActivity  {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
+
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		 // TODO Auto-generated method stub  
+	    if(keyCode == KeyEvent.KEYCODE_BACK)  
+	       {    
+	           exitBy2Click();      //调用双击退出函数  
+	       }  
+	    return false;  
+	}
+	
+	
+	/** 
+	 * 双击退出函数 
+	 */  
+	private static Boolean isExit = false;  
+	private void exitBy2Click() {  
+	    Timer tExit = null;  
+	    if (isExit == false) {  
+	        isExit = true; // 准备退出  
+	        Toast.makeText(this, "再按一次退出程序", Toast.LENGTH_SHORT).show();  
+	        tExit = new Timer();  
+	        tExit.schedule(new TimerTask() {  
+	            @Override  
+	            public void run() {  
+	                isExit = false; // 取消退出  
+	            }  
+	        }, 2000); // 如果2秒钟内没有按下返回键，则启动定时器取消掉刚才执行的任务  
+	  
+	    } else {  
+	        finish();  
+	        System.exit(0);  
+	    }
+	}  
 }
